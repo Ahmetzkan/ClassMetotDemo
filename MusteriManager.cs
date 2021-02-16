f446336510d7b645fa8a720c56bdcfb9f11cabd3
Ahmetzkan <70511583+Ahmetzkan@users.noreply.github.com>
2021-02-16 14:32:03 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void Add(Musteri müsteri)
+        {
+            Console.WriteLine(müsteri.Ad + " " + müsteri.Soyad + " kişisi eklendi.");
+        }
+        public void Delete(Musteri müsteri)
+        {
+            Console.WriteLine(müsteri.Ad + " " + müsteri.Soyad + " kişisi silindi.");
+        }
+        public void Listele(Musteri müsteri)
+        {
+            Console.WriteLine(müsteri.Ad + " " + müsteri.Soyad + " kişisi listelendi.");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyad = "Özkan";
+            musteri1.KimlikNumarasi = 12345;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ali";
+            musteri2.Soyad = "Özkan";
+            musteri2.KimlikNumarasi = 12335;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Ayşe";
+            musteri3.Soyad = "Özkan";
+            musteri3.KimlikNumarasi = 12343;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+            Console.WriteLine("---------------------");
+
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+            musteriManager.Delete(musteri3);
+
+            Console.WriteLine("---------------------");
+
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+
+
+
+
+
+
+        }
+    }
+}
